810f51a14891cc6958ec2a005685225a11d9722c
tayebie <42505961+tayebie@users.noreply.github.com>
2020-02-20 12:00:30 +0330

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,14 +6,13 @@ $(function(){
 
 	$('#slide-submenu').on('click',function() {
         $(this).closest('.list-group').fadeOut('slide',function(){
-        	$('#menuIcon').toggleClass('fa-bars fa-times');
 
         });
 
       });
 
 	$('.mini-submenu').on('click',function(){
-        $(this).next('.list-group').toggle('slide');
+        $('#menuList').toggle('slide');
         $('#menuIcon').toggleClass('fa-bars fa-times');
 })
 });
