3e4c6f015b758871853462e52cc9f7a9ff64ccba
Ali Tayebi <42505961+tayebie@users.noreply.github.com>
2021-04-23 16:33:50 +0430

    test login page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,16 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+function makeid(length) {
+    var result           = [];
+    var characters       = 'ABCDEFGHJKMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz23456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result.push(characters.charAt(Math.floor(Math.random() *
+ charactersLength)));
+   }
+   return result.join('');
+}
+
 $("#menu-toggle").click(function (e) {
   e.preventDefault();
   $("#wrapper").toggleClass("toggled");
